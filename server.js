fadd23943836ef1910cb13c0f3139672fe6fd17e
Harshit <raj.harshitsinha08@gmail.com>
2024-07-23 23:43:43 +0530

    changing in server file

--- server.js
+++ server.js
@@ -9,29 +9,15 @@ app.use(cors());
 app.get("/", async (req, res) => {
   try {
     const options = JSON.parse(req.query.options);
-
-    // const options = {
-    //   method: "GET",
-    //   headers: {
-    //     accept: "application/json",
-    //     authorization:
-    //       "Bearer d035adbfa78f3c420af7cd3c6c12637047fc29b39a4a9772b33224a999fb95df",
-    //   },
-    // };
-
-    const response = await fetch(
-      `https://api.webflow.com/v2/sites/669ca79ffbea6888c8cd512c/collections`,
-      options
-    );
+    const url = req.query.request_url;
+    const response = await fetch(`${url}`, options);
 
     if (!response.ok) {
-      console.log("here");
       const errorText = await response.text();
       throw new Error(
         `HTTP error! status: ${response.status}, message: ${errorText}`
       );
     }
-    console.log("here");
     const data = await response.json();
     res.json(data);
   } catch (error) {
