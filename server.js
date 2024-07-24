b1d71cc3c75d705946334348b708ab623b374e3a
Webyansh <webyansh@gmail.com>
2024-07-24 21:41:23 +0530

    Adding new route

--- server.js
+++ server.js
@@ -8,7 +8,32 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post("/", async (req, res) => {
+app.get("/", async (req, res) => {
+  try {
+    const url = req.query.request_url;
+    const requestOptions = {
+      method: "GET",
+    };
+
+    const response = await fetch(`${url}`, requestOptions);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorText}`
+      );
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({
+      error: error.message || "An error occurred while fetching data",
+    });
+  }
+});
+
+app.post("/webflow-api", async (req, res) => {
   try {
     const options = req.body.options;
     const url = req.body.request_url;
