036b2376ddd228c4e0fe6b5fc789fe1fb8a20981
webyansh <webyansh@gmail.com>
2024-09-06 11:12:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,29 @@ app.post("/webflow-api", async (req, res) => {
   }
 });
 
+app.post("/others", async (req, res) => {
+  try {
+    const options = req.body.options;
+    const url = req.body.request_url;
+
+    const response = await fetch(`${url}`, options);
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${errorText}`
+      );
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error:", error.message);
+    res.status(500).json({
+      error: error.message || "An error occurred while fetching data",
+    });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
